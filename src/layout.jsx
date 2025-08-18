5ffed4ed687ea54898917f05cbb176c8855081f9
harshparmar2073 <115105289+harshparmar2073@users.noreply.github.com>
2025-08-18 20:55:45 +0530

    background fixes

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,23 +1,12 @@
 import Box from "@mui/material/Box";
-import { Outlet, useLocation } from "react-router-dom";
+import { useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import { Container, GlobalStyles } from "@mui/material";
 
 export default function Layout() {
-  const { pathname } = useLocation();
-
-  const defaultGradient = "linear-gradient(to bottom, #FFF3E6 0%, #FFFFFF 100%)";
-  const landingGradient = "linear-gradient(to bottom, #FFE7D0 0%, #FFFFFF 100%)";
-
-  const activeGradient = pathname === "/landing" ? landingGradient : defaultGradient;
   return (
     <>
-      <GlobalStyles
-        styles={{
-          body: {
-            background: activeGradient,
-          },
-        }}
-      />
+      <GlobalStyles styles={{ body: { backgroundColor: "transparent" } }} />
       <Container
         component="main"
         maxWidth="xxl"
